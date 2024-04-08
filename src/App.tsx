aa228404737cd0cba201c32f48e1e59e6bb2553c
devJiraynor <jiraynor@naver.com>
2024-04-08 09:51:56 +0900

    2024.04.08. 09:50

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Component from './component/Component';
 import Sample from './component/Sample';
-import SignInContainer1 from './component/example/SignInContainer';
+import SignInContainer from './component/example/SignInContainer';
 import CurlyBraces from './component/CurlyBraces';
 import Properties from './component_manage/Properties';
-import SignInContainer from './component_manage/example/SignInContainer';
+import SignUpContainer from './component_manage/example/SignUpContainer';
 import ConditionalRendering from './component_manage/ConditionalRendering';
 import ListRendering from './component_manage/ListRendering';
+import EventComponent from './interaction/EventComponent';
 
 // JSX / TSX : 
 // JSX (JavaScript XML) / TSX (TypeScript XML)
@@ -77,11 +78,12 @@ function App() {
       <FunctionComponent /> */}
       {/* <Component />
       <Sample /> */}
-      <SignInContainer />
+      {/* <SignUpContainer /> */}
       {/* <CurlyBraces /> */}
       {/* <Properties /> */}
       {/* <ConditionalRendering /> */}
       {/* <ListRendering /> */}
+      <EventComponent />
     </>
   );
 }
